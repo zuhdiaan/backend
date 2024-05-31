c5cebcef2af7174493d67c63014018ce6d3f4b2e
Zuhdi <farhanzuhdiaan@gmail.com>
2024-05-31 17:14:20 +0700

    api post menu item web server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// server.js
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -26,6 +30,45 @@ app.get('/api/menu_items', (req, res) => {
       res.status(500).json({ error: 'Failed to fetch menu items' });
     } else {
       res.json(results);
+    }
+  });
+});
+
+app.post('/api/menu_items', (req, res) => {
+  const { name, price, image_source, category } = req.body;
+  const sql = 'INSERT INTO menu_items (name, price, image_source, category) VALUES (?, ?, ?, ?)';
+  connection.query(sql, [name, price, image_source, category], (err, result) => {
+    if (err) {
+      console.error('Error inserting into database:', err);
+      res.status(500).json({ error: 'Failed to add menu item' });
+    } else {
+      res.json({ id: result.insertId });
+    }
+  });
+});
+
+
+// Endpoint untuk mendapatkan semua pesanan
+app.get('/api/orders', (req, res) => {
+  let sql = 'SELECT * FROM orders';
+  connection.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Failed to fetch orders' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Endpoint untuk membuat pesanan baru
+app.post('/api/orders', (req, res) => {
+  const { menu_id, table_number, payment_method } = req.body;
+  const sql = 'INSERT INTO orders (menu_id, table_number, payment_status, payment_method, timestamp) VALUES (?, ?, "unpaid", ?, NOW())';
+  connection.query(sql, [menu_id, table_number, payment_method], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: 'Failed to create order' });
+    } else {
+      res.json({ id: result.insertId });
     }
   });
 });
