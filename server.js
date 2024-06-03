58cd57e7b4288516fd902411e51c4f72c6cab753
Zuhdi <farhanzuhdiaan@gmail.com>
2024-06-03 18:50:56 +0700

    backend img

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3000;
+const multer  = require('multer')
+const upload = multer({ dest: 'public/uploads/' })
+const path = require('path');
+const fs = require('fs').promises;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -117,17 +123,46 @@ app.get('/api/menu_items', (req, res) => {
   });
 });
 
-app.post('/api/menu_items', (req, res) => {
-  const { name, price, image_source, category } = req.body;
-  const sql = 'INSERT INTO menu_items (name, price, image_source, category) VALUES (?, ?, ?, ?)';
-  connection.query(sql, [name, price, image_source, category], (err, result) => {
-    if (err) {
-      console.error('Error inserting into database:', err);
-      res.status(500).json({ error: 'Failed to add menu item' });
-    } else {
-      res.json({ id: result.insertId });
-    }
-  });
+app.post('/api/menu_items', upload.single('avatar'), async (req, res) => {
+  try {
+    const { name, price, category } = req.body;
+    const uploadedFile = req.file;
+
+    // Sanitize the filename
+    const sanitizedFilename = name.toLowerCase().replace(/\s+/g, '-');
+    const originalExtension = path.extname(uploadedFile.originalname);
+
+    // Create the target directory if it doesn't exist
+    const targetDirectory = path.join(__dirname, 'public', 'uploads');
+    await fs.mkdir(targetDirectory, { recursive: true });
+
+    // Move the file to the target directory with the sanitized filename and original extension
+    const targetPath = path.join(targetDirectory, sanitizedFilename + originalExtension);
+    await fs.rename(uploadedFile.path, targetPath);
+
+    console.log('File moved successfully');
+
+    // Insert menu item into the database
+    const sql = 'INSERT INTO menu_items (name, price, image_source, category) VALUES (?, ?, ?, ?)';
+    connection.query(sql, [name, price, sanitizedFilename + originalExtension, category], (err, result) => {
+      if (err) {
+        console.error('Error inserting into database:', err);
+        res.status(500).json({ error: 'Failed to add menu item' });
+      } else {
+        res.json({ id: result.insertId });
+      }
+    });
+  } catch (err) {
+    console.error('Error handling request:', err);
+    res.status(500).json({ error: 'Failed to process the request' });
+  }
+});
+
+app.get('/api/files/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const baseUrl = `${req.protocol}://${req.get('host')}`;
+  const fileUrl = `${baseUrl}/uploads/${filename}`;
+  res.json({ url: fileUrl });
 });
 
 app.listen(port, () => {
